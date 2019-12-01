6df70bb2b68466914c7e4cf2b9f1fa63422e4758
Brandon Hills <brandon_hills@icloud.com>
2019-12-01 17:59:53 -0500

    Added an EmptySquare component instead of a blank td tag. Updated Square component with ternary operator to assign a class of today to the square representing the actual day.

---
+++ src/components/empty-square.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function EmptySquare() {
+    return (
+        <button className="date-container">
+        </button>
+    )
+}
+
+export default EmptySquare;
--- src/components/square.js
+++ src/components/square.js
@@ -1,42 +1,16 @@
 import React from 'react';
 
 function Square(props) {
-    const blocksArray = [];
-    let i;
-    for (i = 0; props.blocksInMonth > i; ++i) {
-        blocksArray.push({
-            key: i,
-            day: Number(i - props.firstDayPosition)
-        });
-    }
-
-    const emptyBlock = '<td></td>';
-    const week = emptyBlock.repeat(Number(props.firstDayPosition));
-    console.log(props.firstDayPosition);
-    console.log(week);
-
-    blocksArray.map(day => {
-        if (blocksArray.day <= 0) {
-            return blocksArray.day = null
-        } else {
-            return blocksArray.day = day
-        }
-    });
-
-    console.log(blocksArray);
-    const button = blocksArray.map(block => {
-        return (
-            <td key={blocksArray.key}>
-                <button className="date-container" onMouseOver={props.onMouseOver} onMouseOut={props.onMouseOut}>
-                    {blocksArray[block]}
-                </button>
-            </td>
-        )
-    });
     return (
-        <table>
-            {button}
-        </table>
+        props.today === true ? (
+            <button className="date-container today" onMouseOver={props.onMouseOver} onMouseOut={props.onMouseOut}>
+                {props.keyId}
+            </button>
+        ) : (
+            <button className="date-container" onMouseOver={props.onMouseOver} onMouseOut={props.onMouseOut}>
+                {props.keyId}
+            </button>
+        )
     )
 }
 
