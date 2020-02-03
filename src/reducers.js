100d63957f50b92cb3a0e9f6c61f3d09acb1b47b
Brandon Hills <brandon_hills@icloud.com>
2020-02-03 16:49:56 -0500

    Added proper reducer logic for month decrement

--- src/reducers.js
+++ src/reducers.js
@@ -4,20 +4,25 @@ function rootReducer(state, action) {
     switch (action.type) {
         case 'CHANGE_MONTH_INCREMENT':
             let nextMonth = state.date.add(1, 'M');
-            let newDateObj = {
+            let nextDateObj = {
                 date: nextMonth,
                 month: nextMonth.format('M'),
                 monthLong: nextMonth.format('MMMM'),
                 year: nextMonth.format('YYYY'),
                 daysInMonth: nextMonth.daysInMonth()
             };
-            return Object.assign({}, state, newDateObj);
+            return Object.assign({}, state, nextDateObj);
         case 'CHANGE_MONTH_DECREMENT':
             console.log(`decrement run`);
             let prevMonth = state.date.subtract(1, 'M');
-            return Object.assign({}, state, {
-                date: prevMonth
-            });
+            let prevDateObj = {
+                date: prevMonth,
+                month: prevMonth.format('M'),
+                monthLong: prevMonth.format('MMMM'),
+                year: prevMonth.format('YYYY'),
+                daysInMonth: prevMonth.daysInMonth()
+            };
+            return Object.assign({}, state, prevDateObj);
         default:
             console.log(`default run`);
             return state;
