4baec9273e50d0e2e7a558595407c43d4bd65393
Brandon Hills <brandon_hills@icloud.com>
2020-02-03 13:58:14 -0500

    reducers.js was not saved before last commit

--- src/reducers.js
+++ src/reducers.js
@@ -3,8 +3,6 @@ console.log('reducer fire');
 function rootReducer(state, action) {
     switch (action.type) {
         case 'CHANGE_MONTH_INCREMENT':
-            console.log(`increment run`);
-            console.log(state);
             let nextMonth = state.date.add(1, 'M');
             let newDateObj = {
                 date: nextMonth,
@@ -13,8 +11,6 @@ function rootReducer(state, action) {
                 year: nextMonth.format('YYYY'),
                 daysInMonth: nextMonth.daysInMonth()
             };
-            console.log('newDateObj is ' + newDateObj.daysInMonth);
-            console.log(state);
             return Object.assign({}, state, newDateObj);
         case 'CHANGE_MONTH_DECREMENT':
             console.log(`decrement run`);
